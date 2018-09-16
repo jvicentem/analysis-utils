89c6616e265c85874931988188887d24c8e8dfe5
jvicentem <j.vicentem@alumnos.urjc.es>
2018-09-16 17:44:54 +0200

    Created examples and fixed bugs

--- eda_utils.py
+++ eda_utils.py
@@ -4,30 +4,73 @@ import numpy as np
 from minepy import MINE
 import pandas as pd
 
-def linear_correlation_plot(df, plot_size = (20, 20)):
-    corr = df.corr()
+'''
+Correlation plot given a dataframe. Only the upper triangle is shown.
 
-    df_cols = list(df.columns)
+You can choose the plot size (plot_size) and the correlation method/coefficient to use:
+
+Linear correlation:
+- pearson (by default)
+- kendall
+- spearman
+
+Other kinds of correlation:
+- mic (https://livebook.datascienceheroes.com/exploratory_data_analysis/correlation.html#adding-noise)
+- mic-r2 (https://livebook.datascienceheroes.com/exploratory_data_analysis/correlation.html#measuring-non-linearity-mic-r2)
+
+(https://en.wikipedia.org/wiki/Maximal_information_coefficient)
+(https://www.ncbi.nlm.nih.gov/pubmed/22174245)
+(https://cran.r-project.org/web/packages/minerva/minerva.pdf)
+'''
+def correlation_plot(df, plot_size = (20, 20), method = 'pearson'):
+    if method in ['pearson', 'kendall', 'spearman']:
+        corr = df.corr(method = method)
+    elif method == 'mic':
+        corr = mic(df)
+    elif method == 'mic-r2':
+        corr = mic_r2(df)
+
+    df_cols = list(df.columns)    
 
     plt.figure(figsize=plot_size)
-    sns.heatmap(np.triu(corr, k=1), xticklabels=df_cols, yticklabels=df_cols, annot=True, fmt='.2f')
+
+    drop_self = np.zeros_like(corr)
+    drop_self[np.tril_indices_from(drop_self, k = -1)] = True
+
+    ax = sns.heatmap(corr, xticklabels = df_cols, yticklabels = df_cols, 
+                annot = True, fmt = '.2f', cmap = 'RdBu_r', mask = drop_self
+    )
+
+    ax.set_title(method, fontsize = 20)
+
+    plt.show()
 
     return corr    
 
-def calculate_mic(df):
+def mic(df):
     mine = MINE(alpha = 0.6, c = 15, est = 'mic_approx')
     
     mic_dict = {}
     
-    cols = list(df.columns)
+    for col_a in list(df.columns):
+        mic_dict[col_a] = []
+        
+        for col_b in list(df.columns):
+            mine.compute_score(df[col_a].values, df[col_b].values)
+            mic_dict[col_a].append( mine.mic() )
+    
+    return pd.DataFrame(data = mic_dict, index = df.columns)
+
+def mic_r2(df):    
+    mine = MINE(alpha = 0.6, c = 15, est = 'mic_approx')
+    
+    mic_dict = {}
     
     for col_a in list(df.columns):
         mic_dict[col_a] = []
         
-        for col_b in cols:
+        for col_b in list(df.columns):
             mine.compute_score(df[col_a].values, df[col_b].values)
             mic_dict[col_a].append( mine.mic() )
-
-    cols.remove(col_a)
     
-    return pd.DataFrame(data = mic_dict, index = df.columns)
+    return pd.DataFrame(data = mic_dict, index = df.columns) - np.abs(df.corr())
